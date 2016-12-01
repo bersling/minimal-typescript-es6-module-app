68f582e55a63f9be739778d94087233b2073d25c
Daniel Niederberger <bersling@gmail.com>
2016-12-01 12:44:18 +0100

    Delete some-es6-module.js

--- some-es6-module.js
+++
@@ -1,9 +0,0 @@
-System.register([], function(exports_1) {
-    var someVar;
-    return {
-        setters:[],
-        execute: function() {
-            exports_1("someVar", someVar = "Hello World");
-        }
-    }
-});
